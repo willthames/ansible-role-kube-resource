8b9b883f1bd189dc2266302158c243636691dee0
Will Thames <will@thames.id.au>
2019-05-06 14:47:54 +1000

    Update to latest k8s module_utils for 2.7 role

--- module_utils/k8s/common.py
+++ module_utils/k8s/common.py
@@ -20,11 +20,13 @@ from __future__ import absolute_import, division, print_function
 import copy
 import json
 import os
+import traceback
 
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.dict_transformations import recursive_diff
 from ansible.module_utils.six import iteritems, string_types
+from ansible.module_utils._text import to_native
 
 try:
     import kubernetes
@@ -100,18 +102,23 @@ AUTH_ARG_SPEC = {
     'password': {
         'no_log': True,
     },
-    'verify_ssl': {
+    'validate_certs': {
         'type': 'bool',
+        'aliases': ['verify_ssl'],
     },
-    'ssl_ca_cert': {
+    'ca_cert': {
         'type': 'path',
+        'aliases': ['ssl_ca_cert'],
     },
-    'cert_file': {
+    'client_cert': {
         'type': 'path',
+        'aliases': ['cert_file'],
     },
-    'key_file': {
+    'client_key': {
         'type': 'path',
+        'aliases': ['key_file'],
     },
+    'proxy': {},
 }
 
 
@@ -138,11 +145,13 @@ class K8sAnsibleMixin(object):
         auth = copy.deepcopy(auth_params)
 
         # If authorization variables aren't defined, look for them in environment variables
-        for key, value in iteritems(auth_params):
-            if key in auth_args and value is None:
-                env_value = os.getenv('K8S_AUTH_{0}'.format(key.upper()), None)
+        for arg in auth_args:
+            if auth_params.get(arg) is None:
+                env_value = os.getenv('K8S_AUTH_{0}'.format(arg.upper()), None)
                 if env_value is not None:
-                    auth[key] = env_value
+                    if AUTH_ARG_SPEC[arg].get('type') == 'bool':
+                        env_value = env_value.lower() not in ['0', 'false', 'no']
+                    auth[arg] = env_value
 
         def auth_set(*names):
             return all([auth.get(name) for name in names])
